a93ee295764b43c26c3ee9b92502324661642429
shandytp <shandytsalasa@gmail.com>
2021-03-24 10:44:05 +0700

    finish apply otsu, sauvola, niblack to dataset

--- fix_code.py
+++ fix_code.py
@@ -4,11 +4,11 @@ from skimage.filters import threshold_otsu, threshold_niblack, threshold_sauvola
 import matplotlib.pyplot as plt
 import numpy as np
 
-DATA_PATH = 'path/to/file'
+DATA_PATH = 'brain_tumor_output/val/yes/'
 
-base_dir = 'path/to/file'
+base_dir = 'D:/Kuliah baru/SMT 8/Coding Skripsi'
 train = os.path.join(base_dir, DATA_PATH)
-segmented_dir = os.path.join(base_dir, 'path/to/file')
+segmented_dir = os.path.join(base_dir, 'brain_tumor_fix/sauvola/val/yes')
 
 for r, d, f in os.walk(train):
     for file in f:
@@ -18,16 +18,43 @@ for r, d, f in os.walk(train):
             
             image = cv2.imread(imagePath, 0)
             image = cv2.resize(image, (224,224))
-            thresh_niblack = threshold_niblack(image, window_size=25, k=0.8)
-            thresh_niblack = cv2.ximgproc.niBlackThreshold(image, maxValue=255, type=cv2.THRESH_BINARY_INV, blockSize=2*11+1, k=-0.2, binarizationMethod=cv2.ximgproc.BINARIZATION_NICK)
 
-            binary_niblack = image > thresh_niblack
+            res = image.copy()
+            '''
+            Sauvola Thresholding
+            '''
+            thresh_sauvola = threshold_sauvola(image, window_size=25)
+            thresh_sauvola = cv2.ximgproc.niBlackThreshold(image, maxValue=255, type=cv2.THRESH_BINARY_INV, blockSize=2*11+1, k=-0.2, binarizationMethod=cv2.ximgproc.BINARIZATION_SAUVOLA)
+
+            binary_sauvola = image > thresh_sauvola
+            for i in range(res.shape[0]):
+                for j in range(res.shape[1]):
+                    if binary_sauvola[i][j]==True:
+                        res[i][j]=255
+                    else:
+                        res[i][j]=0
+
+            '''
+            Niblack Thresholding
+            '''
+            # thresh_niblack = threshold_niblack(image, window_size=25, k=0.8)
+            # thresh_niblack = cv2.ximgproc.niBlackThreshold(image, maxValue=255, type=cv2.THRESH_BINARY_INV, blockSize=2*11+1, k=-0.2, binarizationMethod=cv2.ximgproc.BINARIZATION_NICK)
+
+            # binary_niblack = image > thresh_niblack
+            # for i in range(res.shape[0]):
+            #     for j in range(res.shape[1]):
+            #         if binary_niblack[i][j]==True:
+            #             res[i][j]=255
+            #         else:
+            #             res[i][j]=0
+
+            # print(res)
             
             '''
             Otsu Thresholding
             '''
             # image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
             
-            cv2.imwrite(filename = os.path.join(segmented_dir, file), img=np.float32(binary_niblack))
+            cv2.imwrite(filename = os.path.join(segmented_dir, file), img=res)
 
 
